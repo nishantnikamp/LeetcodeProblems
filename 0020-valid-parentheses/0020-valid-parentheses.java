0f2c08155318d4aac79f12d7f7f968ec3fda0396
Nishant Pandharinath Nikam <nishantnikam.p@gmail.com>
2026-07-17 15:16:50 +0530

    Time: 3 ms (87.05%), Space: 43.2 MB (58.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char top = stack.pop();
-                if (ch == ')' && top != '(') {
-                    return false;
-                }
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-                if (ch == '}' && top != '{') {
-                    return false;
-                }
+        Stack<Character> dummy = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='('|| c=='[' || c== '{'){
+                dummy.push(c);
             }
-        }
-        return stack.isEmpty();
+            else{
+                if(dummy.isEmpty()){
+                    return false;
+                }
+                char top = dummy.pop();
+                if(c==')' && top!= '(')
+                    return false;
+                if(c=='}' && top!= '{')
+                    return false;
+                if(c==']' && top!= '[')
+                    return false;
+            }
+        } 
+        return dummy.isEmpty();
     }
 }
