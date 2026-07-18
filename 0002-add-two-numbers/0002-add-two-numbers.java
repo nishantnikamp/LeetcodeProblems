ba6ab457770733fdad78fcbb19db91abf5e99c71
Nishant Pandharinath Nikam <nishantnikam.p@gmail.com>
2026-07-18 22:07:46 +0530

    Time: 1 ms (100%), Space: 46.6 MB (38.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -15,8 +15,8 @@ class Solution {
         int total = 0, carry = 0;
 
         while (l1 != null || l2 != null || carry != 0) {
-            total = carry;
 
+            total = carry;
             if (l1 != null) {
                 total += l1.val;
                 l1 = l1.next;
@@ -30,7 +30,8 @@ class Solution {
             carry = total / 10;
             dummy.next = new ListNode(num);
             dummy = dummy.next;
+
         }
-        return res.next;        
+        return res.next;
     }
 }
