5ee2b170c64cf70a387bf9ad2c1141e926318fee
Nishant Pandharinath Nikam <nishantnikam.p@gmail.com>
2026-08-11 12:18:07 +0530

    Time: 0 ms (100%), Space: 43.9 MB (77.96%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,21 +1,35 @@
 class Solution {
     public int missingInteger(int[] nums) {
-     int sum =nums[0],n =nums.length;
-     for(int i= 1; i<n;i++){
-        if(nums[i] == nums[i-1]+1){
-            sum +=nums[i];
+        int sum = nums[0], n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
+            }
         }
-        else{
-            break;
-        }        
-     }
-     ArrayList<Integer> al = new ArrayList<>();
-     for(int num :nums){
-        al.add(num);
-     }
-     while(al.contains(sum)) 
-        sum++; 
-     return sum;
+        int ans = sum;
+
+        while (true) {
+            boolean found = false;
+            for (int num : nums) {
+                if (ans == num) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return ans;
+            }
+            ans++;
+        }
+        //  ArrayList<Integer> al = new ArrayList<>();
+        //  for(int num :nums){
+        //     al.add(num);
+        //  }
+        //  while(al.contains(sum)) 
+        //     sum++; 
+        //  return sum;
     }
 
 }
